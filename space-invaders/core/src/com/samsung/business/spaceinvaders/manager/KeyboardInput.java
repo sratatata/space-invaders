6ac76d2ce88f758fb682e3fa0c96be5b681936cb
Wojciech Zarski <wojciech.zarski@gmail.com>
2018-01-27 16:13:42 +0100

    Add keyborad for fire and movment
    Fix: #21
    
    Introduced InputManager and GameInputMethod so it would be easier to
    configure how to provide input into the game.
    
    Previously only keyboard movment and touch interface was supported.
    Currently only keayboard interface is supported.

---
+++ space-invaders/core/src/com/samsung/business/spaceinvaders/manager/KeyboardInput.java
@@ -0,0 +1,30 @@
+package com.samsung.business.spaceinvaders.manager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+/**
+ * Created by lb_lb on 27.01.18.
+ */
+public class KeyboardInput implements GameInputMethod{
+    @Override
+    public boolean left(){
+        return Gdx.input.isKeyPressed(Input.Keys.LEFT);
+    }
+
+    @Override
+    public boolean right(){
+        return Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+    }
+
+    @Override
+    public boolean fire(){
+        return Gdx.input.isKeyPressed(Input.Keys.SPACE);
+    }
+
+    @Override
+    public boolean exit(){
+        //todo quit the game;
+        return false;
+    }
+}
